61196a05d7995be9c79101b4124a57850195feb2
Dana Woodman <dana@danawoodman.com>
2023-02-06 13:52:32 -0500

    Finish migration

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script>
 	let transactions = 10;
 	let average_price = 600_000;
 	let total_commission = 3;
@@ -82,6 +82,8 @@
 
 <style lang="postcss">
 	main {
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 		display: flex;
 		flex-direction: row;
 		gap: 2rem;
